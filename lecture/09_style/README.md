4c3dbc8d353b6c7f870b2cba668d3044f73aeae9
Hayeong <90609686+choihayeong@users.noreply.github.com>
2024-11-12 10:58:02 +0900

    customizing video controller

--- lecture/09_style/README.md
+++ lecture/09_style/README.md
@@ -3,3 +3,9 @@
 > font awesome 이용
 
 - [CDN : font awesome](https://cdnjs.com/libraries/font-awesome)
+
+## Custom Video Controller
+
+- [HTMLMediaElement: loadedmetadata event](https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/loadedmetadata_event)
+
+- [HTMLMediaElement: timeupdate event](https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/timeupdate_event)
--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,9 +1,14 @@
 const videoEl = document.querySelector("video");
 
 const playBtnEl = document.getElementById("play");
-const muteEl = document.getElementById("mute");
-const timeEl = document.getElementById("time");
-const volumeEl = document.getElementById("volume");
+const muteBtnEl = document.getElementById("mute");
+const volumeRangeEl = document.getElementById("volume");
+const currentTimeEl = document.getElementById("currentTime");
+const totalTimeEl = document.getElementById("totalTime");
+const timelineEl = document.getElementById("timeline");
+
+let initVolume = 0.5;
+videoEl.volume = initVolume;
 
 const clickPlayBtn = (e) => {
   if (videoEl.paused) {
@@ -11,13 +16,63 @@ const clickPlayBtn = (e) => {
   } else {
     videoEl.pause();
   }
+
+  playBtnEl.innerText = videoEl.paused ? "Play" : "Pause";
 };
 const handleVideoPause = () => (playBtnEl.innerText = "Play");
 const handleVideoPlay = () => (playBtnEl.innerText = "Pasue");
 
-const handleMute = (e) => {};
+const clickMuteBtn = (e) => {
+  if (videoEl.muted) {
+    videoEl.muted = false;
+  } else {
+    videoEl.muted = true;
+  }
+
+  muteBtnEl.innerText = videoEl.muted ? "Unmute" : "Mute";
+  volumeRangeEl.value = videoEl.muted ? 0 : initVolume;
+};
+
+const changeVolumeController = (e) => {
+  const {
+    target: { value },
+  } = e;
+
+  if (videoEl.muted) {
+    videoEl.muted = false;
+    muteBtnEl.innerText = "Mute";
+  }
+
+  initVolume = value;
+  videoEl.volume = value;
+};
+
+const formatTimer = (seconds) =>
+  new Date(seconds * 1000).toISOString().substring(11, 19); // 야매 방법임 ㅋ
+
+const loadedMetaDataVideo = () => {
+  totalTimeEl.innerText = formatTimer(Math.floor(videoEl.duration));
+  timelineEl.max = Math.floor(videoEl.duration);
+};
+
+const timeUpdateVideo = () => {
+  currentTimeEl.innerText = formatTimer(Math.floor(videoEl.currentTime));
+  timelineEl.value = Math.floor(videoEl.currentTime);
+};
+
+const changeTimeLineController = (e) => {
+  const {
+    target: { value },
+  } = e;
+
+  videoEl.currentTime = value;
+};
 
 playBtnEl.addEventListener("click", clickPlayBtn);
-muteEl.addEventListener("click", handleMute);
+muteBtnEl.addEventListener("click", clickMuteBtn);
 videoEl.addEventListener("pause", handleVideoPause);
 videoEl.addEventListener("play", handleVideoPlay);
+volumeRangeEl.addEventListener("input", changeVolumeController);
+videoEl.addEventListener("loadedmetadata", loadedMetaDataVideo);
+videoEl.addEventListener("timeupdate", timeUpdateVideo);
+timelineEl.addEventListener("input", changeTimeLineController);
