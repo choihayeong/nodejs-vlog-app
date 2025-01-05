73dbc27c5e0ae08e9444d05d8510e0e93c997543
Hayeong <hychoi318@gmail.com>
2025-01-05 22:37:32 +0900

    setting for comments api

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -6,6 +6,18 @@ const submitComment = (event) => {
   const textareaEl = formEl.querySelector("textarea");
   const text = textareaEl.value;
   const videoId = inputVideoId.value;
+
+  if (text === "") {
+    return alert("공백은 댓글로 입력할 수 없습니당");
+  }
+
+  fetch(`/api/videos/${videoId}/comment`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ text }),
+  });
 };
 
 if (formEl) {
